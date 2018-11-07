676b6d360664397555a62c91ff3b659b80c1af16
Joshua Adams <joshadams554@gmail.com>
2018-11-07 15:52:45 -0500

    slightly modular

---
+++ src/Calculations.js
@@ -0,0 +1,64 @@
+export const yearCode = (input)=>{
+  const YY = parseInt(input.slice(2,4)); 
+  return ((YY+ Math.floor(YY/4))%7) 
+}
+
+export const monthCode = (input) =>{
+  let returnMonth = 6; 
+ 
+  switch(input){
+    case '01':
+    case '10':
+     returnMonth = 0;
+     break; 
+    case '05':
+     returnMonth = 1;
+     break; 
+    case '08':
+     returnMonth = 2;
+     break; 
+    case '02':
+    case '03':
+    case '11':
+     returnMonth = 3;
+     break; 
+    case '06':
+     returnMonth = 4;
+     break; 
+    case '09':
+    case '12':
+     returnMonth = 5;
+     break; 
+    default:
+     returnMonth = 6; 
+  }
+  return returnMonth
+}
+
+export const centuryCode = (input) =>{
+  const YY = parseInt(input.slice(0,2)); 
+  switch(YY){
+    case 19:
+    case 23: 
+     return 0;
+    case 22:
+    case 18:
+     return 2; 
+    case 17:
+    case 21: 
+     return 4; 
+    default:
+     return 6; 
+  }
+}
+
+export const leapYear = (input) => {
+  const leap = parseInt(input.slice(0,4));
+
+  if (leap % 400 === 0){
+    return 1
+  } else if (leap % 4 === 0 && leap % 100 !== 0) {
+    return 1
+  } else 
+  return 0 
+};
--- src/DayByDate.js
+++ src/DayByDate.js
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import InputForm from './InputForm';
 import OutputField from './OutputField';
+import { yearCode, monthCode, centuryCode, leapYear } from './Calculations';
 
 export default class DayByDate extends React.Component{
    constructor(props) {
@@ -14,75 +15,18 @@ export default class DayByDate extends React.Component{
     console.log(input);
     this.setState({date: input});   
    }
-   yearCode(input){
-     const YY = parseInt(input.slice(2,4)); 
-     return ((YY+ Math.floor(YY/4))%7) 
-   }
-   monthCode(input){
-     let returnMonth = 6; 
-    
-     switch(input){
-       case '01':
-       case '10':
-        returnMonth = 0;
-        break; 
-       case '05':
-        returnMonth = 1;
-        break; 
-       case '08':
-        returnMonth = 2;
-        break; 
-       case '02':
-       case '03':
-       case '11':
-        returnMonth = 3;
-        break; 
-       case '06':
-        returnMonth = 4;
-        break; 
-       case '09':
-       case '12':
-        returnMonth = 5;
-        break; 
-       default:
-        returnMonth = 6; 
-     }
-     return returnMonth
-   }
-   centuryCode(input){
-     const YY = parseInt(input.slice(0,2)); 
-     switch(YY){
-       case 19:
-       case 23: 
-        return 0;
-       case 22:
-       case 18:
-        return 2; 
-       case 17:
-       case 21: 
-        return 4; 
-       default:
-        return 6; 
-     }
-   }
-   leapYear(input){
-     const leap = parseInt(input.slice(0,4));
-
-     if (leap % 400 === 0){
-       return 1
-     } else if (leap % 4 === 0 && leap % 100 !== 0) {
-       return 1
-     } else 
-     return 0 
-   };
+  
+   
+   
+   
    calcDateVal(date){
-     const yearCode = this.yearCode(date);
-     const monthCode = this.monthCode(date.slice(5,7));
-     const centuryCode = this.centuryCode(date);
-     const leapYear = this.leapYear(date); 
-     const dayCode = parseInt(date.slice(8,10)); 
+     const _yearCode = yearCode(date);
+     const _monthCode = monthCode(date.slice(5,7));
+     const _centuryCode = centuryCode(date);
+     const _leapYear = leapYear(date); 
+     const _dayCode = parseInt(date.slice(8,10)); 
 
-     return ((yearCode + monthCode + centuryCode + dayCode - leapYear)%7)
+     return ((_yearCode + _monthCode + _centuryCode + _dayCode - _leapYear)%7)
    };
    calcDay(){
     const dayValue = this.calcDateVal(this.state.date);
